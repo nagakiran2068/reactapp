4e65b1dd52490f4329af45c931a564f7c898a5a4
nagtech <cfmx.kiran@gmail.com>
2022-11-07 20:27:09 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import HeaderNav from "./components/Header";
 
 export default function App() {
   return (
     <div className="App">
+      <HeaderNav />
       <h1>Hello CodeSandbox</h1>
-      <h2> editing to some happen!</h2>
+      <h2 className="red"> editig to some happen!</h2>
     </div>
   );
 }
