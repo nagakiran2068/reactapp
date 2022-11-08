8ff9bb9f30881056d5b2bcd91dd91c6ea0715ee9
nagakiran2068 <41209342+nagakiran2068@users.noreply.github.com>
2022-11-08 10:17:07 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import "./styles.css";
 import Greet from "./Components/Greet";
 import Welcome from "./Components/Welcome";
+import Message from "./Components/Message";
+import Counter from "./Components/Counter";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Greet name="Anusha">
+      {/* <h1>Hello CodeSandbox</h1> */}
+      {/* <h2>Start editing to see some magic happen!</h2> */}
+      {/* <Greet name="Anusha">
         <p>Hello Children</p>
       </Greet>
       <Greet name="Naga" />
       <Greet name="Eeshan" />
       <Greet name="Sanavi" />
 
-      <Welcome />
-      <Welcome />
-      <Welcome />
+      <Welcome name="Naga" />
+      <Welcome name="Anu" />
+      <Welcome name="Sanu" /> */}
+
+      {/* <Message /> */}
+
+      <Counter />
     </div>
   );
 }
---
+++ src/Components/Counter.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  //set state
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Count {this.state.count}</h1>
+        <button onClick={() => this.increment()}>Increment</button>
+      </div>
+    );
+  }
+}
+export default Counter;
