e38d55668fc9bcbdfcf3f12eff58954a2ca2a69b
nagakiran2068 <41209342+nagakiran2068@users.noreply.github.com>
2022-11-08 07:57:05 -0500

    fgfgfgfgfgg fggfgfgfg

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./styles.css";
 import Greet from "./Components/Greet";
+import Welcome from "./Components/Welcome";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <Greet />
+      <Greet name="Anusha">
+        <p>Hello Children</p>
+      </Greet>
+      <Greet name="Naga" />
+      <Greet name="Eeshan" />
+      <Greet name="Sanavi" />
+
+      <Welcome />
+      <Welcome />
+      <Welcome />
     </div>
   );
 }
